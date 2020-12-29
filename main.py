238cb02516156abba5df3f558c283ec35acdeb57
= <=>
2020-12-29 13:48:19 -0500

    version 3, completed delete last character

--- main.py
+++ main.py
@@ -4,31 +4,59 @@ window = Tk()
 window.title("Simple Calculator")
 
 text = Entry(window, font=("Calibri 22"))
-text.grid(row=0, column=0, columnspan=5, pady=7, padx=10)
+text.grid(row=0, column=0, columnspan=5, pady=10, padx=20)
+i = 0
+#functions
+def get_number(value):
+    global i
+    text.insert(i, value)
+    i += 1
+def clear_screen():
+    text.delete(0, END)
+    global i
+    i = 0
+def calculate():
+    try:
+        operation = text.get()
+        result = eval(operation)
+        text.delete(0,END)
+        text.insert(0, result)
+    except:
+        text.delete(0,END)
+        text.insert(0, "Error")
+        global i
+        i = 0
+
+def delete_last():
+    state = text.get()
+    new = state[:-1]
+    text.delete(0,END)
+    text.insert(0,new)
+
 
 # Buttons - numbers
-button1 = Button(window, text="1", width =5, height=2)
-button2 = Button(window, text="2", width =5, height=2)
-button3 = Button(window, text="3", width =5, height=2)
-button4 = Button(window, text="4", width =5, height=2)
-button5 = Button(window, text="5", width =5, height=2)
-button6 = Button(window, text="6", width =5, height=2)
-button7 = Button(window, text="7", width =5, height=2)
-button8 = Button(window, text="8", width =5, height=2)
-button9 = Button(window, text="9", width =5, height=2)
-button0 = Button(window, text="0", width =5, height=2)
+button1 = Button(window, text="1", width =5, height=2, command = lambda: get_number(1))
+button2 = Button(window, text="2", width =5, height=2, command = lambda: get_number(2))
+button3 = Button(window, text="3", width =5, height=2, command = lambda: get_number(3))
+button4 = Button(window, text="4", width =5, height=2, command = lambda: get_number(4))
+button5 = Button(window, text="5", width =5, height=2, command = lambda: get_number(5))
+button6 = Button(window, text="6", width =5, height=2, command = lambda: get_number(6))
+button7 = Button(window, text="7", width =5, height=2, command = lambda: get_number(7))
+button8 = Button(window, text="8", width =5, height=2, command = lambda: get_number(8))
+button9 = Button(window, text="9", width =5, height=2, command = lambda: get_number(9))
+button0 = Button(window, text="0", width =5, height=2, command = lambda: get_number(0))
 
 # Buttons - operators
-button_AC = Button(window, text="AC", width =5, height=2)
-button_point = Button(window, text=".", width =5, height=2)
-button_par1 = Button(window, text="(", width =5, height=2)
-button_par2 = Button(window, text=")", width =5, height=2)
-button_sum = Button(window, text="+", width =5, height=2)
-button_minus = Button(window, text="-", width =5, height=2)
-button_product = Button(window, text="x", width =5, height=2)
-button_division = Button(window, text="÷", width =5, height=2)
-button_result = Button(window, text="=", width =5, height=2)
-button_retro = Button(window, text="del", width =5, height=2)
+button_AC = Button(window, text="AC", width =5, height=2, command = lambda :clear_screen())
+button_point = Button(window, text=".", width =5, height=2, command = lambda: get_number("."))
+button_par1 = Button(window, text="(", width =5, height=2, command = lambda: get_number("("))
+button_par2 = Button(window, text=")", width =5, height=2, command = lambda: get_number(")"))
+button_sum = Button(window, text="+", width =5, height=2, command = lambda: get_number("+"))
+button_minus = Button(window, text="-", width =5, height=2, command = lambda: get_number("-"))
+button_product = Button(window, text="x", width =5, height=2, command = lambda: get_number("*"))
+button_division = Button(window, text="÷", width =5, height=2, command = lambda: get_number("/"))
+button_result = Button(window, text="=", width =5, height=2, command = lambda: calculate())
+button_retro = Button(window, text="del", width =5, height=2, command=lambda: delete_last())
 
 # row 1
 button7.grid(row= 1, column = 0 , padx= 5, pady= 5)
